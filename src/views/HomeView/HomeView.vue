fc70df18ef1a8984a6773c23075667d88e2dc14f
szcleiton <souzacleiton54@gmail.com>
2024-01-28 12:02:14 -0300

    Update job title and description in various places

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -12,7 +12,7 @@
         <div class="skills container">
             <h1>{{ globalObject.skillsTitle }}</h1>
             <p>
-                Java / X++ / Ruby / JS / Rust / TypeScript / Node.js / Python
+                Java • Python • JavaScript • TypeScript • React • Angular • Svelte • Node • Express • Django • SpringBoot • AWS • Docker • among others
             </p>
             <div class="btn-container">
                 <download-button @click="downloadCv()" class="">Download CV</download-button>
